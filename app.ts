e1223c660d3da3c214b20a7fe023c71b4eda4166
Mindaugas Jurgelaitis <mindaugas@stekas.lt>
2020-03-28 21:28:20 +0200

    Add search endpoint

--- app.ts
+++ app.ts
@@ -19,31 +19,83 @@ app.listen(port, () => console.log(`Server running on port ${port}`))
 app.get("/", (req, res, next) => res.send("Privacy Vs Germs"))
 
 class Location {
-    timeFrom: number
-    timeTo: number
-    latitude: number
-    longitude: number
+    coordinates: Coordinates
+    time: Time
 
-    constructor(
-        timeFrom: number,
-        timeTo: number,
-        latitude: number,
-        longitude: number) {
-        this.timeFrom = timeFrom
-        this.timeTo = timeTo
-        this.latitude = latitude
-        this.longitude = longitude
+    constructor(coordinates: Coordinates, time: Time) {
+        this.coordinates = coordinates
+        this.time = time
+    }
+}
+
+class Time {
+    from: number
+    to: number
+
+    constructor(from: number, to: number) {
+        this.from = from
+        this.to = to
+    }
+}
+
+class Coordinates {
+    lat: number
+    lon: number
+
+    constructor(lat: number, lon: number) {
+        this.lat = lat
+        this.lon = lon
     }
 }
 
 const locationsIndex = "locations"
 
-app.post("/locations", (req, res) => {
-    const locations: Location[] = req.body
-    const body = locations.flatMap(location => [{ index: { _index: locationsIndex } }, location])
-    elasticClient.bulk({ refresh: "true", body: body }, (err, resp) => res.send())
+app.post("/locations/setup-index", async (req, res) => {
+    await elasticClient.indices.create({
+        index: locationsIndex,
+        body: {
+            mappings: {
+                properties: {
+                    coordinates: { type: "geo_point" },
+                    time: {
+                        properties: {
+                            from: { type: "integer" },
+                            to: { type: "integer" }
+                        }
+                    }
+                }
+            }
+        }
+    }, { ignore: [400] })
+    res.send()
 })
 
-app.get("/locations", (req, res) => {
-    elasticClient.search({ index: locationsIndex }, (err, resp) => res.send(resp.body.hits.hits))
+app.post("/locations", async (req, res) => {
+    const locations: Location[] = req.body
+    const body = locations.flatMap(location => [{ index: { _index: locationsIndex } }, location])
+    await elasticClient.bulk({ refresh: "true", body: body })
+    res.send()
+})
+
+app.get("/locations", async (req, res) => {
+    const locations: Location[] = req.body
+    const result = await elasticClient.search({
+        index: locationsIndex,
+        body: {
+            query: {
+                bool: {
+                    should: locations.map(location => ({
+                        geo_distance: {
+                            distance: "10m",
+                            coordinates: {
+                                lat: location.coordinates.lat,
+                                lon: location.coordinates.lon
+                            }
+                        }
+                    }))
+                }
+            }
+        }
+    })
+    res.send(result)
 })
