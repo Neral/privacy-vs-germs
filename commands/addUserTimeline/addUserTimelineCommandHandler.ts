186762cb022416759eaed7bfaa670c307ac70022
Mindaugas <jmindaugas17@yahoo.co.uk>
2020-04-11 00:05:16 +0300

    Add check if user exists

--- commands/addUserTimeline/addUserTimelineCommandHandler.ts
+++ commands/addUserTimeline/addUserTimelineCommandHandler.ts
@@ -10,7 +10,7 @@ export class AddUserTimelineCommandHandler {
 
     public async Handle(command: AddUserTimelineCommand): Promise<void> {
 
-        const timelineId = Guid.create().toString()
+        let timelineId = Guid.create().toString()
         await transformAndValidate(AddUserTimelineCommand, command)
 
         const env = process.env
@@ -21,21 +21,31 @@ export class AddUserTimelineCommandHandler {
             password: env.RDS_PASSWORD
         })
 
-        connection.query(`INSERT into Users (guid, email) values ('${timelineId}', '${command.email}')`, (err) => {
-            if (err) throw err;
+        connection.query(`SELECT guid from Users where email = '${command.email}'`, (err, rows) => {
+            if (err) console.log(err)
+            if (rows.length==0) {
+                connection.query(`INSERT into Users (guid, email) values ('${timelineId}', '${command.email}')`, (err) => {
+                    if (err) console.log(err)
+                });
+            }
+            else {
+                timelineId = rows[0].guid
+            }
+
+            this.client.bulk({
+                refresh: "true",
+                body: command.locations
+                    .map(location => new UserLocation(
+                        timelineId,
+                        command.testType,
+                        command.testDate,
+                        [location.longitude, location.latitude],
+                        location.timeFrom,
+                        location.timeTo))
+                    .flatMap(location => [{ index: { _index: this.index } }, location])
+            })
+            
         });
-        
-        await this.client.bulk({
-            refresh: "true",
-            body: command.locations
-                .map(location => new UserLocation(
-                    timelineId,
-                    command.testType,
-                    command.testDate,
-                    [location.longitude, location.latitude],
-                    location.timeFrom,
-                    location.timeTo))
-                .flatMap(location => [{ index: { _index: this.index } }, location])
-        })
+
     }
 }
