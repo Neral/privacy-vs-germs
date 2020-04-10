15773665b4cc1de10fd095da5a4e2f17f1d8de8f
Mindaugas <jmindaugas17@yahoo.co.uk>
2020-04-10 21:12:02 +0300

    Added mysql connection

--- commands/addUserTimeline/addUserTimelineCommandHandler.ts
+++ commands/addUserTimeline/addUserTimelineCommandHandler.ts
@@ -1,23 +1,35 @@
 import { Guid } from "guid-typescript"
-import { sha256 } from "js-sha256"
 import { AddUserTimelineCommand } from "./addUserTimelineCommand"
 import { Client } from "@elastic/elasticsearch"
 import { UserLocation } from "../../elastic/userLocation"
 import { transformAndValidate } from "class-transformer-validator"
+import mysql from "mysql"
 
 export class AddUserTimelineCommandHandler {
     constructor(private client: Client, private index: string) { }
 
     public async Handle(command: AddUserTimelineCommand): Promise<void> {
-        await transformAndValidate(AddUserTimelineCommand, command)
+
         const timelineId = Guid.create().toString()
-        const emailHash = sha256(command.email)
+        await transformAndValidate(AddUserTimelineCommand, command)
+
+        const env = process.env
+        const connection = mysql.createConnection({
+            host: env.RDS_HOSTNAME,
+            database : env.RDS_DB_NAME,
+            user: env.RDS_USERNAME,
+            password: env.RDS_PASSWORD
+        })
+
+        connection.query(`INSERT into Users (guid, email) values ('${timelineId}', '${command.email}')`, (err) => {
+            if (err) throw err;
+        });
+        
         await this.client.bulk({
             refresh: "true",
             body: command.locations
                 .map(location => new UserLocation(
                     timelineId,
-                    emailHash,
                     command.testType,
                     command.testDate,
                     [location.longitude, location.latitude],
